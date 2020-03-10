a0babeb0184b502c186bc5d8c08ddf41bb40f9db
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-10 09:30:38 +0100

    add charOccurence method

--- characterOccurence/characterOccurence.js
+++ characterOccurence/characterOccurence.js
@@ -1,6 +1,8 @@
 // You are given an input string.
 
- //For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+ //For each symbol in the string if it's the first character occurence,
+ // replace it with a '1', else replace it with the amount of times you've
+ // already seen it...
 
  // But will your code be performant enough
  // Examples
@@ -10,3 +12,22 @@
 
  // input   =  aaaaaaaaaaaa
  // result  =  123456789101112
+
+function charOccurence(str) {
+	let occurence_arr = new Array(256).fill(0);
+	let res = new Array(str.length);
+	for (var i=0; i<str.length; i++) {
+		let char = str.charCodeAt(i);
+		occurence_arr[char - 1]++;
+		if (char > 96 && char < 123 || char > 64 && char < 91) {
+			if (char < 91) {
+				res[i] = occurence_arr[char - 1] + occurence_arr[char + 31];
+			} else {
+				res[i] = occurence_arr[char - 1] + occurence_arr[char - 33];
+			}
+		} else {
+			res[i] = occurence_arr[char - 1];
+		}
+	}
+	return res.join('');
+}
