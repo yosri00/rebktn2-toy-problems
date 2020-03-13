6a2160cc0bb495fb7d8ec0a95bd78b3b7b211652
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-13 09:43:44 +0100

    add isSubsetOf method

--- isSubSetOf/isSubSetOf.js
+++ isSubSetOf/isSubSetOf.js
@@ -15,10 +15,41 @@
  *
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
-*/
+ */
 /*
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
-*/
-Array.prototype.isSubsetOf = function (arr) {
+ */
+ Array.prototype.includes = function (element) {
+ 	for(var i=0; i<this.length; i++) {
+ 		if (typeof(this[i]) == 'object') {
+ 			if (Array.isArray(this[i])) {
+ 				if (this[i].includes(element)) {
+ 					return true;
+ 				}
+ 			} else if (Object.values(this[i]).includes(element)){
+ 				return true;
+ 			}
+ 		} else if (this[i] === element) {
+ 			return true;
+ 		}
+ 	}
+ 	return false;
+ }
+ Array.prototype.isSubsetOf = function (arr) {
     // your code here
+    
+    for(var i=0; i<this.length; i++) {
+    	if (typeof(this[i]) == 'object') {
+    		if (Array.isArray(this[i])) {
+    			if(!this[i].isSubsetOf(arr)) {
+    				return false;
+    			}
+    		} else if (!Object.values(this[i]).isSubsetOf(arr)){
+    			return false;
+    		}
+    	} else if (!arr.includes(this[i])) {
+    		return false;
+    	}
+    }
+    return true;
 };
