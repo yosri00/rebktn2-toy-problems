4b999372019e545135478471a9458480efb2b5a1
yosri00 <max.power1212222.com>
2020-03-25 09:46:25 +0100

    yosri

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,30 @@
  */
 
 var binarySearch = function (array, target) {
+    
+        var len = array.length;
+        var mid = array[Math.floor(len/2)];
+        
+    
+        
+        if (array.length === 1 && array[0] !== target) {
+            return null
+        }
+    
+    
+        if(target < array[0] || target > array[len - 1]) {
+            return null
+        }
+    
+        if(target === mid) {
+            return Math.floor(len/2)
+        }
+        else if(target < mid) {
+            
+            return binarySearch(array.slice(0, Math.floor(len/2) + 1), target)
+        } else if (target > mid) {
+            
+            return binarySearch(array.slice(Math.floor(len/2) ), target) 
 };
+}
 
