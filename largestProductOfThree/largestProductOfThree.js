ce484110611f054e753e152a351aa8c7ee5336f5
yosri00 <max.power1212222.com>
2020-03-25 09:41:05 +0100

    yosri

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,22 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+function largestProductOfThree(array) {
+        if(array.length < 3) {
+            return "Not enough elems"
+        }
+    
+        var max = array[0]*array[1]*array[2];
+    
+        for (var i = 0; i < array.length; i++) {
+            for (var j = i+1; j < array.length ; j++) {
+                for (var k = j+1; k < array.length; k++) {
+                    if(array[i]*array[j]*array[k] > max) {
+                        max = array[i]*array[j]*array[k];
+                    }
+                }
+            }	
+        }
+    
+        return max
+}
