afd7a58131bf8fdf1b0182a2faa2647658fbe4a9
yosri00 <max.power1212222.com>
2020-03-26 10:58:19 +0100

    yosri

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -9,4 +9,29 @@ parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello wo
 parseQueryString("http://example.com") // undefined
 */
 
-function parseQueryString(url) {}
+function parseQueryString(url) {
+        var buket = []
+        var test = ["?","%","="]
+    
+        if (url.indexOf("?") === -1) {
+            return undefined
+        }
+        res = url.slice(url.indexOf("?") + 1)
+        var tab = res.split("&")
+        for (var i = 0; i < tab.length; i++){
+            helper(tab[i])
+        }
+       
+    }
+    function helper(element) {
+        var test = ["?", "%", "="]
+        var tab = element.split("")
+        
+    
+        for (var i = 0; i < test.length; i++){
+            if (element.includes(test[i])) {
+                console.log(test[i])
+                var result = element.replace(test[i],' ')
+            } 
+   }
+}
