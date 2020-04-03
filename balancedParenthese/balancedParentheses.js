9f4cdaa406e3708309d425cbe4cab1c58bf26d77
yosri00 <max.power1212222.com>
2020-04-03 11:06:10 +0200

    yosri

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,16 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+	var num = 0;
+	
+	for (var i = 0; i <str.length; i++) {
+		if(str[i] === "(") {
+			num++
+		} else if(str[i] === ")") {
+			num--
+		}
+	}
+
+	return num === 0;
+};
