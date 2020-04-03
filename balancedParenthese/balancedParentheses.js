0d70a11bef852597415a4f38398b49972c74b92b
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-04-03 08:29:09 +0100

    balance

---
+++ balancedParenthese/balancedParentheses.js
@@ -0,0 +1,11 @@
+/*
+Balanced Parenthesis
+Given a string, return true if it contains balanced parentheses ().
+
+isBalanced("(x + y) - (4)")	// true
+isBalanced("(((10 ) ()) ((?)(:)))")	// true
+isBalanced("(50)(")	// false
+isBalanced("") //	true
+*/
+
+var isBalanced = function(str) {};
